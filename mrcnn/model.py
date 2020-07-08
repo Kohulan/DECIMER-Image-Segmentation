e2c90e0469972bb76cbbbc6e5ef67634cfd668af
Kohulan Rajan <kohulan.rajan@uni-jena.de>
2020-07-08 16:40:07 +0530

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -5,6 +5,9 @@ The main Mask R-CNN model implementation.
 Copyright (c) 2017 Matterport, Inc.
 Licensed under the MIT License (see LICENSE for details)
 Written by Waleed Abdulla
+
+Modified on 2020 July by : Kohulan Rajan 
+
 """
 
 import os
