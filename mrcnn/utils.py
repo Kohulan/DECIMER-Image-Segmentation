7104f8d41f7c5dc1d2fd42c246f0732b994a44b0
Kohulan Rajan <kohulan.rajan@uni-jena.de>
2020-07-08 16:39:05 +0530

    Update utils.py

--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -5,6 +5,8 @@ Common utility functions and classes.
 Copyright (c) 2017 Matterport, Inc.
 Licensed under the MIT License (see LICENSE for details)
 Written by Waleed Abdulla
+
+Modified on 2020 July by : Kohulan Rajan 
 """
 
 import sys
