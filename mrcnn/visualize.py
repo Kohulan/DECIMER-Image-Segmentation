549294a4210d6e9d706a60f8c840c5e2bef48fb2
Kohulan Rajan <kohulan.rajan@uni-jena.de>
2020-07-08 16:38:48 +0530

    Update visualize.py

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -5,6 +5,8 @@ Display and Visualization Functions.
 Copyright (c) 2017 Matterport, Inc.
 Licensed under the MIT License (see LICENSE for details)
 Written by Waleed Abdulla
+
+Modified on 2020 July by : Kohulan Rajan 
 """
 
 import os
