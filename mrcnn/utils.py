12b4690ca6d9a6bb28a96b7ce4e89219985c3a17
Kohulan <kohulan.rajan@uni-jena.de>
2020-07-13 16:59:57 +0200

    Output directory creation updated

--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -26,7 +26,7 @@ import warnings
 from distutils.version import LooseVersion
 
 # URL from which to download the latest COCO trained weights
-COCO_MODEL_URL = "https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5"
+COCO_MODEL_URL = "https://github.com/Kohulan/DECIMER-Image-Segmentation/blob/master/model_trained/mask_rcnn_molecule.h5"
 
 
 ############################################################
