4513f5186de3f8baf313ca076e057040b12fb7c9
OBrink <brinkhaus@gmail.com>
2020-11-30 16:24:18 +0100

    Removed multiprocessing/ray from expansion; Consequent use of typing for code readability; Deleted zombie-code;

--- Scripts/complete_structure.py
+++ Scripts/complete_structure.py
@@ -7,7 +7,6 @@ import itertools
 from PIL import Image
 from matplotlib.patches import Polygon
 from scipy.ndimage.filters import gaussian_filter
-from scipy import ndimage
 
 from skimage.color import rgb2gray
 from skimage.filters import threshold_otsu
@@ -16,10 +15,7 @@ from skimage import img_as_float, img_as_int
 from imantics import Mask, Polygons
 from typing import Dict, List, Tuple
 
-from multiprocessing import Pool
-
-
-def plot_it_multiple(image_array, bounding_boxes):
+def plot_it_multiple(image_array: np.array, bounding_boxes: np.array) -> None:
 	'''This function shows the plot of a given image (np.array([...])) with a given list of bounding boxes np.array([[x1, y1], [x2, y2],...,[x_n, y_n]])'''
 	fig,ax = plt.subplots(1)
 	imgplot = ax.imshow(image_array)
@@ -31,7 +27,7 @@ def plot_it_multiple(image_array, bounding_boxes):
 	plt.show()
 	return
 
-def plot_it(image_array, bounding_box):
+def plot_it(image_array: np.array, bounding_box: np.array) -> None:
 	'''This function shows the plot of a given image (np.array([...])) with a given bounding box np.array([[x1, y1], [x2, y2],...,[x_n, y_n]])'''
 	bounding_box = Polygon(bounding_box, linewidth=1,edgecolor='r',facecolor='none')
 	fig,ax = plt.subplots(1)
@@ -41,7 +37,7 @@ def plot_it(image_array, bounding_box):
 	plt.show()
 	return
 
-def define_bounding_box_center(bounding_box):
+def define_bounding_box_center(bounding_box: np.array) -> np.array:
 	'''This function return the center np.array([x,y]) of a given bounding box np.array([[x1, y1], [x2, y2],...,[x_n, y_n]])'''
 	x_values = np.array([])
 	y_values = np.array([])
@@ -50,35 +46,27 @@ def define_bounding_box_center(bounding_box):
 		y_values = np.append(y_values, node[1])
 	return np.array([np.mean(x_values), np.mean(y_values)])
 
-def define_edge_line(node_1, node_2):
-	'''This function returns a linear function between two given points in a 2-dimensional space'''
-	#if node_1[0]-node_2[0] != 0:
+def define_edge_line(node_1: Tuple[int, int], node_2: Tuple[int, int]) -> Tuple[float, float]:
+	'''This function returns slope and intercept of a linear function between two given points in a 2-dimensional space'''
 	slope = (node_2[1]-node_1[1])/(node_2[0]-node_1[0])
-	#else:
-	#	slope = (node_2[1]-node_1[1])/(node_2[0]-node_1[0]+0.00000000000001) # avoid dividing by zero, this line should not be necesary anymore.
-	#intercept = y - m*x
 	intercept = node_1[1] - slope * node_1[0]
 	return slope, intercept
 
-def euklidian_distance(node_1, node_2):
+def euklidian_distance(node_1: Tuple[int, int], node_2: Tuple[int, int]) -> float:
 	'''This function returns the euklidian distance between two given points in a 2-dimensional space.'''
 	return np.sqrt((node_2[0]-node_1[0]) ** 2 + (node_2[1]-node_1[1])**2)
     
-def set_x_diff_range(x_diff, euklidian_distance, image_array):
+def set_x_diff_range(x_diff: float, euklidian_distance: float, image_array: np.array) -> np.array:
 	'''This function takes the amount of steps on an edge and returns the corresponding list of steps in random order'''
 	blur_factor = int(image_array.shape[1]/500) if image_array.shape[1]/500 >= 1 else 1
 	if x_diff > 0:
-		x_diff_range = np.arange(0, x_diff, blur_factor/euklidian_distance)
-		#x_diff_range = np.arange(0, x_diff, 00.1) 
-		
+		x_diff_range = np.arange(0, x_diff, blur_factor/euklidian_distance)		
 	else:
 		x_diff_range = np.arange(0, x_diff, -blur_factor/euklidian_distance)
-		#x_diff_range = np.arange(0, x_diff, -00.1)
-		
 	np.random.shuffle(x_diff_range)
 	return x_diff_range
 
-def define_next_pixel_to_check(bounding_box, node_index, step, image_shape):
+def define_next_pixel_to_check(bounding_box: np.array, node_index: int, step: int, image_shape: Tuple[int, int, int]) -> Tuple[int, int]:
 	'''This function returns the next pixel to check in the image (along the edge of the bounding box). 
 	In the case that it tries to check a pixel outside of the image, this is corrected.'''
 	# Define the edges of the bounding box
@@ -95,10 +83,11 @@ def define_next_pixel_to_check(bounding_box, node_index, step, image_shape):
 		x = 0
 	return x,y
 
-def adapt_x_values(bounding_box, node_index, image_shape):
+def adapt_x_values(bounding_box: np.array, node_index: int, image_shape: Tuple[int, int, int]) -> np.array:
 	'''If two nodes form a vertical edge the function that descripes that edge will in- or decrease infinitely with dx so we need to alter those nodes. 
 	This function returns a bounding box where the nodes are altered depending on their relative position to the center of the bounding box.
 	If a node is at the border of the image, then it is not changed.'''
+	bounding_box = copy.deepcopy(bounding_box)
 	if bounding_box[node_index][0] != image_shape[1]:
 		bounding_box_center = define_bounding_box_center(bounding_box)
 		if bounding_box[node_index][0] < bounding_box_center[0]:
@@ -107,10 +96,10 @@ def adapt_x_values(bounding_box, node_index, image_shape):
 			bounding_box[node_index][0] = bounding_box[node_index][0] + 1
 	return bounding_box
 
-def mask_2_polygons(mask_array):
+def mask_2_polygons(mask_array: np.array) -> np.array:
 	'''This function takes a mask array and returns a list of polygon bounding boxes (node coordinates)'''
-	
-	#imantics is built on openCV. The mask needs to be inverted so that the findcontours function works.
+
+	#imantics is built on openCV. The mask needs to be inverted so that the findcontours function works. Unintuitive but it works this way.
 	inverted_mask = np.invert(mask_array).astype(int)
 	#split the mask array with all masks into n arrays with shape (x,y,1) (one mask_array per molecule)
 	polygons = []
@@ -120,7 +109,7 @@ def mask_2_polygons(mask_array):
 		polygons.append(Polygons.from_mask(current_mask).points)
 	return polygons
 
-def binarize_image(image_array, threshold = "otsu"):
+def binarize_image(image_array: np.array, threshold = "otsu") -> np.array:
 	'''This function takes a Numpy array that represents an RGB image and returns the binarized form of that image
 	by applying the otsu threshold.'''
 	grayscale = rgb2gray(image_array)
@@ -129,7 +118,7 @@ def binarize_image(image_array, threshold = "otsu"):
 	binarized_image_array = grayscale > threshold
 	return binarized_image_array
 
-def define_relevant_polygons(polygons):
+def define_relevant_polygons(polygons: np.array) -> np.array:
 	'''Sometimes, the mask R CNN model produces a weird output with one big masks and some small irrelevant islands.
 	This function takes the imantics Polygon object and returns the Polygon object that only contains the biggest 
 	bounding box (which is defined by the biggest range in y-direction).'''
@@ -146,7 +135,7 @@ def define_relevant_polygons(polygons):
 	return modified_polygons
 
 
-def find_seeds(image_array, bounding_box):
+def find_seeds(image_array: np.array, bounding_box: np.array) -> List[Tuple[int, int]]:
 	'''This function an array that represents an image and a bounding box. 
 	It returns a list of tuples with indices of objects in the image on the bounding box edges.'''
 
@@ -168,7 +157,7 @@ def find_seeds(image_array, bounding_box):
 				#break
 	return seed_pixels
 
-def determine_neighbour_pixels(seed_pixel, image_shape):
+def determine_neighbour_pixels(seed_pixel: Tuple[int, int], image_shape: Tuple[int, int, int]) -> List[Tuple[int, int]]:
 	'''This function takes a tuple of x and y coordinates and returns a list of tuples of the coordinates of the eight neighbour pixels.'''
 	neighbour_pixels = []
 	x,y = seed_pixel
@@ -180,7 +169,7 @@ def determine_neighbour_pixels(seed_pixel, image_shape):
 						neighbour_pixels.append((new_x, new_y))
 	return neighbour_pixels
 
-def expand_masks(image_array, seed_pixels, mask_array):
+def expand_masks(image_array: np.array, seed_pixels: List[Tuple[int, int]], mask_array: np.array) -> np.array:
 	'''This function takes...
 	image_array - Numpy array that represents an image (float)
 	mask_array - Numpy array containing binary matrices that define masks (y, x, mask_index)
@@ -200,7 +189,8 @@ def expand_masks(image_array, seed_pixels, mask_array):
 
 def expansion_coordination(mask_bounding_box_tuple: Tuple[np.array, np.array], image_array: np.array) -> np.array:
 	'''This function takes a tuple containing a bounding box, a single mask and an image (all three: np.array) and coordinates
-	the mask expansion. It returns the expanded mask'''
+	the mask expansion. It returns the expanded mask. 
+	The purpose of this function is wrapping up the expansion procedure in a map function.'''
 	mask_array, bounding_box = mask_bounding_box_tuple
 	seed_pixels = find_seeds(image_array, bounding_box)
 	mask_array = expand_masks(image_array, seed_pixels, mask_array)
@@ -210,43 +200,33 @@ def complete_structure_mask(image_array: np.array, mask_array: np.array, debug =
 	'''This funtion takes an image (array) and an array containing the masks (shape: x,y,n where n is the amount of masks and x and y are the pixel coordinates).
 	It detects objects on the contours of the mask and expands it until it frames the complete object in the image. 
 	It returns the expanded mask array'''
-
+	
 	if mask_array.size != 0:
+
 		# Turn masks into list of polygon bounding boxes
 		polygons = mask_2_polygons(mask_array)
-
+		
+		# Delete unnecessary mask blobs
 		polygons = define_relevant_polygons(polygons)
 
 		#Binarization of input image
 		binarized_image_array = binarize_image(image_array)
 
-		# Apply gaussian filter to the image and binarize again
+		# Apply gaussian filter with a resolution-dependent standard deviation to the image 
 		blur_factor = int(image_array.shape[1]/500) if image_array.shape[1]/500 >= 1 else 1
-		
 		blurred_image_array = gaussian_filter(img_as_float(binarized_image_array).copy(), sigma=blur_factor)
 
 		# Create list tuples of masks and corresponding bounding_box
-		mask_bounding_box_tuples = [(mask_array[:,:,index], polygons[index][0]) for index in range(len(polygons))]
+		mask_bounding_box_tuples = np.array([(mask_array[:,:,index], polygons[index][0]) for index in range(len(polygons))])		
 		
-		# Run expansion using Multiprocessing
-		image_repeat = itertools.repeat(blurred_image_array, len(mask_bounding_box_tuples)) # Why does this not work?
-		#image_repeat = [copy.deepcopy(blurred_image_array) for _ in range(len(mask_bounding_box_tuples))]
-
-		#expanded_split_mask_arrays = map(expansion_coordination, mask_bounding_box_tuples, image_repeat)
-		with Pool(20) as p:
-			expanded_split_mask_arrays = p.starmap(expansion_coordination, zip(mask_bounding_box_tuples, image_repeat))
-
+		# Run expansion the expansion
+		image_repeat = itertools.repeat(blurred_image_array, len(mask_bounding_box_tuples))
+		expanded_split_mask_arrays = map(expansion_coordination, mask_bounding_box_tuples, image_repeat)
+		
 		# Stack mask arrays to give the desired output format
 		mask_array = np.stack(expanded_split_mask_arrays, -1)
-
-		#for polygon_index in range(len(polygons)): 
-
-		#	bounding_box = polygons[polygon_index][0]
-
-			# Find seed pixels for expansion
-		#	seed_pixels = find_seeds(blurred_image_array, bounding_box)
-		#	mask_array = expand_masks(blurred_image_array, seed_pixels, mask_array, mask_index = polygon_index)
+		return mask_array
 
 	else: 
 		print("No masks found.")
-	return mask_array
+		return mask_array
