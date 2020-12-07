f04f442a03ff1edbcced4bf036cd9d99fc5650cb
OBrink <brinkhaus@gmail.com>
2020-12-07 12:50:42 +0100

    Load model only once instead of loading it repeatedly

--- validation_run_with_time_analysis.py
+++ validation_run_with_time_analysis.py
@@ -48,7 +48,7 @@ def main():
 	args = parser.parse_args()
 	
 
-		MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+	MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 
 	# Local path to trained weights file
 	COCO_MODEL_PATH = os.path.join("model_trained/mask_rcnn_molecule.h5")
