5e6de7b99061e485071526c472c7802ddff3873e
Otto Brinkhaus <brinkhaus@gmail.com>
2022-05-10 15:02:15 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DECIMER Segmentation",
+    version="1.1",
+    author="Kohulan Rajan",
+    author_email="kohulan.rajan@uni-jena.de",
+    maintainer="Kohulan Rajan",
+    maintainer_email="kohulan.rajan@uni-jena.de",
+    description="DECIMER Segmentation - Extraction of chemical structure depictions from scientific literature",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Kohulan/DECIMER-Image-Segmentation",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    install_requires=[
+        "tensorflow==2.3.0"
+        "pillow"
+        "opencv-python"
+        "matplotlib"
+        "imantics"
+        "IPython"
+        "pdf2image"
+    ],
+    package_data={"DECIMER_Segmentation": ["repack/*.*", "trainer/*.*"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.5",
+)
