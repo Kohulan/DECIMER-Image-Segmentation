c0cbe614b246345138522012e6cd6149b26b1f09
Otto Brinkhaus <59618901+OBrink@users.noreply.github.com>
2022-11-07 14:00:13 +0100

    version bump 1.1 --> 1.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="decimer_segmentation",
-    version="1.1",
+    version="1.1.1",
     author="Kohulan Rajan",
     author_email="kohulan.rajan@uni-jena.de",
     maintainer="Kohulan Rajan",
